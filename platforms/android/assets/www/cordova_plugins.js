2b649df2486e260d16a4a79ed6b046bd5a4a4e93
codesajinda <csmasterpiece@gmail.com>
2015-10-03 17:23:40 -0500

    removed whitelist and reinstalled

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -9,7 +9,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.1.1-dev"
 }
 // BOTTOM OF METADATA
 });
